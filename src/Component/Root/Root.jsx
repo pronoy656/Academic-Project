f3ef38fa571cef86116783d79f7cc2ff5b612782
pronoy656 <pronoypaul56@gmail.com>
2024-11-16 18:04:50 +0600

    router and navbar,footer setup done

--- src/Component/Root/Root.jsx
+++ src/Component/Root/Root.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
   return (
     <div>
-      <h1>This is Root</h1>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
